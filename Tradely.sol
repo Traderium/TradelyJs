51dda3e02637951800ef789b8f3abaf056824795
StephenHawkes <42497511+StephenHawkes@users.noreply.github.com>
2018-08-18 18:03:29 +0100

    Update Tradely.sol

--- Tradely.sol
+++ Tradely.sol
@@ -4,7 +4,7 @@ pragma solidity 0.4.18;
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
-library SafeMath {
+library SafeMath {   
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a * b;
         require(a == 0 || c / a == b);
